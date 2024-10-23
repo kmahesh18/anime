e74c60d4b0d6854babe052fb9f46504508bffb51
Pavan507-STAR <pavan317.b@gmail.com>
2024-10-23 21:47:37 +0530

    changes

--- anime/app/ui/Footer.tsx
+++ anime/app/ui/Footer.tsx
@@ -1,23 +1,20 @@
-"use client";
-import Link from "next/link";
 import redditicon from "@/assets/icons8-reddit.svg";
-import telegramicon from "@/assets/icons8-telegram.svg";
+import TelegramIcon from "@/assets/icons8-telegram.svg";
 import discordicon from "@/assets/icons8-discord.svg";
 import xicon from "@/assets/icons8-x.svg";
 import Image from "next/image";
-
 function Footer() {
   const footericons = [
     { name: "discord", href: "https://discord.com/", icon: discordicon },
-    { name: "telegramicon", href: "https://telegram.com/", icon: telegramicon },
+    { name: "telegramicon", href: "https://telegram.com/", icon: TelegramIcon },
     { name: "redditicon", href: "https://reddit.com/", icon: redditicon },
     { name: "xicon", href: "https://x.com/", icon: xicon },
   ];
   return (
     <div>
-      <Link href="/dashboard">
-        <Image src={icon} alt=""></Image>
-      </Link>
+      <button className="p-2 hover:bg-gray-100 rounded-full transition-colors">
+        <TelegramIcon className="w-6 h-6 text-blue-500 hover:text-blue-600" />
+      </button>
     </div>
   );
 }
