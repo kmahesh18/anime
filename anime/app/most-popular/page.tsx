dbdc091dd06a511ccfcef1d4073329b957f50489
arbazahmed07 <mdarbazking7@gmail.com>
2024-10-23 18:22:42 +0530

    files added

---
+++ anime/app/most-popular/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>most poular</div>
+  )
+}
+
+export default page
---
+++ anime/app/top-airing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      top-airing
+    </div>
+  )
+}
+
+export default page
