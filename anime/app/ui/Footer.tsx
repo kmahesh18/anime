649f3da5013d5cd4edcf179a9b1f8171352269f2
Pavan507-STAR <pavan317.b@gmail.com>
2024-10-23 21:32:46 +0530

    changes

--- anime/app/ui/Footer.tsx
+++ anime/app/ui/Footer.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import redditicon from "@/assets/icons8-reddit.svg";
+import telegramicon from "@/assets/icons8-telegram.svg";
+import discordicon from "@/assets/icons8-discord.svg";
+import xicon from "@/assets/icons8-x.svg";
+import Image from "next/image";
 
 function Footer() {
+  const footericons = [
+    { name: "discord", href: "https://discord.com/", icon: discordicon },
+    { name: "telegramicon", href: "https://telegram.com/", icon: telegramicon },
+    { name: "redditicon", href: "https://reddit.com/", icon: redditicon },
+    { name: "xicon", href: "https://x.com/", icon: xicon },
+  ];
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <Link href="/dashboard">
+        <Image src={icon} alt=""></Image>
+      </Link>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
+
